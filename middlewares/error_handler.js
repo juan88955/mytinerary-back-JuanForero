19b43922c8f3d656dd27dbca8509445f06a97bc4
Juan Carlos Forero <juan.forero66@gmail.com>
2024-11-04 19:16:25 -0500

    actualizacion del middleware

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -2,7 +2,8 @@ const error_handler = (error, req, res, next) => {
     console.log(error);
     return res.status(500).json({
         success: false,
-        message: error.message || 'Error interno del servidor'
+        message: 'error',
+        error: error.message || 'Error interno del servidor'
     });
 };
 
